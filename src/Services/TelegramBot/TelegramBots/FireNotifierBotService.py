20760883d7537ccaee031a5d561a6ad38e8723fb
Shevalier <sebastienriv974@gmail.com>
2025-12-08 20:59:46 +0100

    Adding of Telegram Bot control and reorganisation of some folder

---
+++ src/Services/TelegramBot/TelegramBots/FireNotifierBotService.py
@@ -0,0 +1,37 @@
+from src.Services.TelegramBot.TelegramBotService import TelegramBotService
+from src.Services.MQTT.MQTTService import MQTTService
+from src.libs.Telegram.TelegramBot import TelegramBot
+from time import sleep
+
+class FireNotifierBotService(TelegramBotService, MQTTService):
+    
+    def __init__(self, configFilePath:str, token:str) -> None:
+        
+        self.handler = {'chat': self.on_chat_message, 'callback_query': self.on_callback_query}
+        
+        TelegramBotService.__init__(self, configFilePath, token, self.handler)
+        MQTTService.__init__(self, configFilePath)
+        
+    def updateLoopRunTime(self, updateInterval:int=12) -> None :
+        while self.serviceRunTimeStatus :
+            oldCatalog = self.configCatalog.copy()
+            self.updateCatalogConfig()
+            
+            if oldCatalog != self.configCatalog :
+                print("Info: Service catalog updated. Restarting Telegram Bot with new configuration.")
+                self.setupTelegramBot()
+                self.mqttSetupClient()
+                        
+            sleep(self.configCatalog.get("CatalogUpdateIntervalCycles", self.configLocal.get("CatalogUpdateIntervalCycles", updateInterval)))
+        
+    def on_chat_message(self, msg): # Must be defined
+        pass
+    
+    def on_callback_query(self, msg): # Must be defined
+        pass
+    
+    def mqttCallback(self, topic, message) -> None : # Must be defined
+        pass
+    
+    def serviceRunTime(self) -> None : # Must be defined
+        pass
