7c8a2f671388d43e7b5fc5df22393fcab15d2891
Sebastien Riviere <sebastienriv974@gmail.com>
2026-02-05 16:30:54 +0100

    add docker supporte for all servcie, and add some new servcie

--- MicroServices/FireDetectorAlarmDevice/FireDetectorCreator.py
+++ MicroServices/FireDetectorAlarmDevice/FireDetectorCreator.py
@@ -1,6 +1,8 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../..")
+
+# Change the working directory to the project root
+os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
 from src.DevicesConnectors.Device.Devices.FireDetectorDevice import FireDetectorDevice
 from src.DevicesConnectors.SensorsSimulation.ModelSensor.ModelSensors.TemperatureSensorSimulation import TemperatureSensorSimulation
@@ -8,17 +10,19 @@ from src.DevicesConnectors.SensorsSimulation.ModelSensor.ModelSensors.COSensorSi
 from src.DevicesConnectors.SensorsSimulation.ModelSensor.ModelSensors.TVOCSensorSimulation import TVOCSensorSimulation
 from src.DevicesConnectors.SensorsSimulation.Sensors.SmokeSensorSimulation import SmokeSensorSimulation
 
-if __name__ == "__main__" :
-    base_path = os.path.dirname(os.path.abspath(__file__))
-    
-    sensorCO = COSensorSimulation(os.path.join(base_path, "data_set/indoor_data.csv"))
-    sensorTemp = TemperatureSensorSimulation(os.path.join(base_path, "data_set/OFFC_EMY.csv"))
-    sensorTVOC = TVOCSensorSimulation(os.path.join(base_path, "data_set/indoor_data.csv"))
+if __name__ == "__main__":
+    # Use the project root to construct file paths
+    data_set_path = os.path.join(os.getcwd(), "MicroServices/FireDetectorAlarmDevice/data_set")
+    config_path = os.path.join(os.getcwd(), "MicroServices/FireDetectorAlarmDevice/configFireDetectorDevice.yaml")
+
+    sensorCO = COSensorSimulation(os.path.join(data_set_path, "indoor_data.csv"))
+    sensorTemp = TemperatureSensorSimulation(os.path.join(data_set_path, "OFFC_EMY.csv"))
+    sensorTVOC = TVOCSensorSimulation(os.path.join(data_set_path, "indoor_data.csv"))
     sensorSmoke = SmokeSensorSimulation()
-    
-    sensorsArray = [ sensorTemp, sensorTVOC, sensorCO, sensorSmoke ]
-    
-    device = FireDetectorDevice(os.path.join(base_path, "configFireDetectorDevice.yaml"), sensorsArray)
-    
+
+    sensorsArray = [sensorTemp, sensorTVOC, sensorCO, sensorSmoke]
+
+    device = FireDetectorDevice(config_path, sensorsArray)
+
     device.setDeviceRunTimeStatus(True)
     device.deviceRunTime()
