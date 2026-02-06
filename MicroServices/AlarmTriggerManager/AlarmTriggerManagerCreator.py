a1f6cb2fe4c0ce7f99ca32c1aef57a31ac9c945b
Federico Galfione <s312758@studenti.polito.it>
2026-02-06 19:04:07 +0100

    fix

--- MicroServices/AlarmTriggerManager/AlarmTriggerManagerCreator.py
+++ MicroServices/AlarmTriggerManager/AlarmTriggerManagerCreator.py
@@ -2,4 +2,4 @@ from AlarmTriggerManager import AlarmTriggerManager
 if __name__=="__main__":
     alarmTriggerManager=AlarmTriggerManager("configAlarmTriggerManager.yaml")
     alarmTriggerManager.setRunTimeStatus(True)
-    alarmTriggerManager.RunTime()
+    alarmTriggerManager.serviceRunTime()
